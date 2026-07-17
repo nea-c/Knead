ea8d48f2a303ce4d5fe58be27faccc82c4caf877
0kqnet <0kq@0kqnet.work>
2026-07-18 01:06:23 +0900

    ci: add multi-platform release builds

--- builder.ts
+++ builder.ts
@@ -1,38 +1,34 @@
-import { build } from 'electron-builder'
+import type { Configuration } from 'electron-builder'
 
-build({
-  config: {
-    appId: 'com.neac.knead',
-    productName: 'Knead',
-    copyright: 'Copyright © 2025 ${author}',
-    artifactName: '${productName}-${version}-${arch}.${ext}',
-    files: ['dist/**/*'],
-    directories: {
-      output: 'release',
-    },
-    icon: 'build/icon.png',
-    nsis: {
-      oneClick: false,
-      allowToChangeInstallationDirectory: true,
-    },
-    win: {
-      target: ['zip'],
-      publisherName: 'NeAc',
-      icon: 'build/icon.png',
-    },
-    mac: {
-      icon: 'build/icon.icns',
-      target: {
-        target: 'default',
-        arch: 'universal',
-      },
-      // コード署名しない場合は null の設定が必須
-      identity: null,
-    },
-    linux: {
-      icon: 'build/icon.icns',
-      target: ['AppImage'],
-      category: 'Development',
-    },
+const config: Configuration = {
+  appId: 'com.neac.knead',
+  productName: 'Knead',
+  copyright: 'Copyright © 2025 ${author}',
+  artifactName: '${productName}-${version}-${arch}.${ext}',
+  files: ['dist/**/*'],
+  directories: {
+    output: 'release',
   },
-})
+  icon: 'build/icon.png',
+  nsis: {
+    oneClick: false,
+    allowToChangeInstallationDirectory: true,
+  },
+  win: {
+    target: ['zip'],
+    icon: 'build/icon.png',
+  },
+  mac: {
+    icon: 'build/icon.icns',
+    target: ['dmg'],
+    // コード署名しない場合は null の設定が必須
+    identity: null,
+  },
+  linux: {
+    icon: 'build/icon.icns',
+    target: ['AppImage'],
+    category: 'Development',
+  },
+}
+
+export default config
